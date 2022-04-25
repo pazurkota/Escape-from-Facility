afe0d89d68cc85d35c4f10f9c78d2abbb1d2e3f9
Piotr Jelonek <piotrek.jelonek@icloud.com>
2022-04-25 16:58:55 +0200

    Added camera following the player

--- escape-from-facility/Assets/Scripts/PlayerController.cs
+++ escape-from-facility/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject camera;
+    public GameObject Camera;
     
     [SerializeField] private float horizontalSpeed;
     [SerializeField] private float verticalSpeed;
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+        CameraMovement();
     }
 
     // This void is responsible for player movement in X and Z axis
@@ -29,6 +30,13 @@ public class PlayerController : MonoBehaviour
         
         // Move player in vertical Axis
         transform.Translate(Vector3.right * normalSpeed * verticalSpeed * Time.deltaTime);
+        // Move player in horizontal Axis
         transform.Translate(Vector3.back * normalSpeed * horizontalSpeed * Time.deltaTime);
+    }
+
+    // This void is responsible for camera following the player
+    private void CameraMovement()
+    {
+        Camera.transform.position = transform.position + new Vector3(0, 9, 0);
     }
 }
