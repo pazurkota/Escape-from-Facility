8cb90e98745b770e28984bc6e6bb9ff65b9e9e82
Piotr Jelonek <piotrek.jelonek@icloud.com>
2022-04-25 16:55:15 +0200

    Added player movement

--- escape-from-facility/Assets/Scripts/PlayerController.cs
+++ escape-from-facility/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject camera;
+    
+    [SerializeField] private float horizontalSpeed;
+    [SerializeField] private float verticalSpeed;
+    [SerializeField] private int normalSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +17,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+    }
+
+    // This void is responsible for player movement in X and Z axis
+    private void PlayerMovement()
+    {
+        // Set vertical and horizontal inputs
+        verticalSpeed = Input.GetAxis("Vertical");
+        horizontalSpeed = Input.GetAxis("Horizontal");
         
+        // Move player in vertical Axis
+        transform.Translate(Vector3.right * normalSpeed * verticalSpeed * Time.deltaTime);
+        transform.Translate(Vector3.back * normalSpeed * horizontalSpeed * Time.deltaTime);
     }
 }
